1e90c4bcd1dd43ff135b20f466e5d57f9143994c
Ilya Peresadin <pva701@gmail.com>
2015-03-07 00:37:51 +0300

    impelemented read_, write_. Written Makefile

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,7 +1,34 @@
 #include "helpers.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 ssize_t read_(int fd, void *buf, size_t count) {
+    ssize_t bytesRead = 0;
+    while (bytesRead < count) {
+        ssize_t actualRead = read(fd, buf + bytesRead, count - bytesRead);
+        if (actualRead == 0)
+            break;
+        else if (actualRead == -1) {
+            if (errno != EAGAIN && errno != EWOULDBLOCK)
+                break;
+        } else 
+            bytesRead += actualRead;
+    }
+    return bytesRead;
 }
 
 ssize_t write_(int fd, void *buf, size_t count) {
-
+    ssize_t bytesWritten = 0;
+    while (bytesWritten < count) {
+        ssize_t actualWritten = read(fd, buf + bytesWritten, count - bytesWritten);
+        if (actualWritten == -1) {
+            if (errno != EAGAIN && errno != EWOULDBLOCK)
+                break;
+        } else 
+            bytesWritten += actualWritten;
+    }
+    return bytesWritten;
 }    
--- lib/helpers.h
+++ lib/helpers.h
@@ -1,5 +1,8 @@
 #ifndef HELPERS_H
 #define HELPERS_H
+#include <stddef.h>
+#include <stdlib.h>
+
 
 ssize_t read_(int fd, void *buf, size_t count);
 ssize_t write_(int fd, void *buf, size_t count);
