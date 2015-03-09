ae7f16bacac7efcb846b28beb7cd5daffeb22446
Ilya Peresadin <pva701@gmail.com>
2015-03-09 14:26:50 +0300

    added structure CharQueue

--- lib/helpers.c
+++ lib/helpers.c
@@ -30,45 +30,53 @@ ssize_t write_(int fd, void *buf, size_t count) {
 
 
 #define MAX_SIZE 1024
-char q[2 * MAX_SIZE];
-size_t l, r;
-void ensure() {
-    if (l + MAX_SIZE > 2 * MAX_SIZE) {
+struct CharQueue {
+    char data[2 * MAX_SIZE];
+    size_t l, r;
+};
+void ensure(struct CharQueue* q) {
+    if (q->l + MAX_SIZE > 2 * MAX_SIZE) {
         size_t i = 0;
-        for (; i < r - l; ++i)
-            q[i] = q[i + l];
-        r -= l;
-        l = 0;
+        for (; i < q->r - q->l; ++i)
+            q->data[i] = q->data[i + q->l];
+        q->r -= q->l;
+        q->l = 0;
     }
 }
-size_t push(void* bufTo, size_t count, char delimiter) {
+size_t push(struct CharQueue* q, void* bufTo, size_t count, char delimiter) {
     size_t pushed = 0;
     char *bufToChr = (char*)bufTo;
-    for (; pushed < count && q[l + pushed] != delimiter; ++pushed)
-        bufToChr[pushed] = q[l + pushed];
-    l += pushed;
-    ensure();
+    for (; pushed < count && q->data[q->l + pushed] != delimiter; ++pushed)
+        bufToChr[pushed] = q->data[q->l + pushed];
+    q->l += pushed;
+    ensure(q);
     return pushed;
 }
-size_t size() {
-    return r - l;
+size_t size(struct CharQueue* q) {
+    return q->r - q->l;
 }
 
 ssize_t read_until(int fd, void * buf, size_t count, char delimiter) {
+    static struct CharQueue* q;
+    if (q == NULL) {
+        q = (struct CharQueue*)malloc(sizeof (struct CharQueue));
+        q->l = 0;
+        q->r = 0;
+    }
     size_t ptr = 0;
     while (ptr < count) {
         size_t actualRead = -2;
-        if (size() != MAX_SIZE)
-            actualRead = read(fd, q + r, MAX_SIZE - size());
+        if (size(q) != MAX_SIZE)
+            actualRead = read(fd, q->data + q->r, MAX_SIZE - size(q));
         if (actualRead == 0 || actualRead == -1 && errno != EAGAIN && errno != EWOULDBLOCK)
             break;
         else {
             if (actualRead != -2) 
-                r += actualRead;
-            size_t mn = size() < count - ptr ? size() : count - ptr;
-            size_t pushed = push(buf + ptr, mn, delimiter);
+                q->r += actualRead;
+            size_t mn = size(q) < count - ptr ? size(q) : count - ptr;
+            size_t pushed = push(q, buf + ptr, mn, delimiter);
             ptr += pushed;
-            if (size() != 0)
+            if (size(q) != 0)
                 break;
         }
     }
