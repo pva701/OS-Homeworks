be1717280e8c0e022e2c6dbab5fa83895200d9ce
Ilya Peresadin <pva701@gmail.com>
2015-03-10 23:37:53 +0300

    fixed cat.c

--- cat/cat.c
+++ cat/cat.c
@@ -7,9 +7,13 @@ const size_t SIZE_BUFFER = 1024;
 int main() {
     char buf[SIZE_BUFFER];
     while (1) {
-        size_t bytes = read_(STDIN_FILENO, buf, SIZE_BUFFER);
+        ssize_t bytes = read_(STDIN_FILENO, buf, SIZE_BUFFER);
         if (bytes == 0)
             break;
+        if (bytes == -1) {
+            fprintf(stderr, "error");
+            return 0;
+        }
         write_(STDOUT_FILENO, buf, bytes);
     }
     return 0;
