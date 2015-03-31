f7d16a96650453b3c394c343179838f0c46986c0
Ilya Peresadin <pva701@gmail.com>
2015-03-31 18:16:15 +0300

    added DEBUG macros

--- lib/bufio.c
+++ lib/bufio.c
@@ -1,31 +1,45 @@
 #include "bufio.h"
-#include <assert.h>
 #include <string.h>
+
+#ifdef DEBUG
+#define myAssert(x) if (!(x)) abort();
+#else
+#define myAssert(x)
+#endif
 
 struct buf_t *buf_new(size_t capacity) {
     struct buf_t* ret = (struct buf_t*)malloc(sizeof (struct buf_t));
+    if (ret == NULL)
+        return ret;
     ret->capacity = capacity;
     ret->size = 0;
     ret->data = (char*)malloc(sizeof(char) * capacity);
+    if (ret->data == NULL) {
+        free(ret);
+        return NULL;
+    }
     return ret;
 }
 
 void buf_free(struct buf_t* buf) {
+    myAssert(buf != NULL);
     free(buf->data);
     free(buf);
 }
 
 size_t buf_capacity(struct buf_t* buf) {
+    myAssert(buf != NULL);
     return buf->capacity;
 }
 
 size_t buf_size(struct buf_t* buf) {
+    myAssert(buf != NULL);
     return buf->size;
 }
 
 ssize_t buf_fill(fd_t fd, struct buf_t *buf, size_t required) {
-    assert(buf != NULL);
-    assert(required <= buf->capacity);
+    myAssert(buf != NULL);
+    myAssert(required <= buf->capacity);
     buf->size = 0;//TODO uncomment if it requested
     while (buf->size < required) {
         ssize_t bytes = read(fd, buf->data + buf->size, buf->capacity - buf->size);
@@ -39,7 +53,7 @@ ssize_t buf_fill(fd_t fd, struct buf_t *buf, size_t required) {
 }
 
 ssize_t buf_flush(fd_t fd, struct buf_t *buf, size_t required) {
-    assert(buf != NULL);
+    myAssert(buf != NULL);
     size_t prevSize = buf->size;
     while (prevSize - buf->size < required && buf->size != 0) {
         ssize_t bytes = write(fd, buf->data, buf->size);
