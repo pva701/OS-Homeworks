6c7b4a7c978efb486bacd6a7debd64906434e579
Ilya Peresadin <pva701@gmail.com>
2015-06-03 01:01:00 +0300

    done, nah

--- filesender/filesender.c
+++ filesender/filesender.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include "bufio.h"
 #include <string.h>
+#include <fcntl.h>
 
 int startServer(int port) {
     int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -26,26 +27,14 @@ int startServer(int port) {
 }
 
 #define MAX_SIZE_BUFFER 4096
-int main(int argc, char* argv[]) {
+int main(int argc, const char* argv[]) {
     if (argc != 3) {
         printf("Input 3 agruments!\n");
         return 0;
     }
-    int fd = open(argv[2]);
-    printf("fd = %d\n", fd);
-    perror("msg");
-    close(fd);
-    return 0;
-/*    struct buf_t *buf = buf_new(MAX_SIZE_BUFFER);
-    int n = strlen(fileName);
-    char *tmp = malloc(strlen(fileName) + 3);
-    int i = 0;
-    for (; i <= n; ++i) tmp[i + 2] = fileName[i];
-    tmp[0] = '.';
-    tmp[1] = '/';
-    fileName = tmp;
-    printf("s = %s\n", fileName);*/
-    int port = itoa(argv[1]);
+
+    struct buf_t *buf = buf_new(MAX_SIZE_BUFFER);
+    int port = atoi(argv[1]);
     const char* fileName = argv[2];
     int sock = startServer(port);
 	printf("sock = %d\n", sock);
@@ -62,23 +51,11 @@ int main(int argc, char* argv[]) {
         printf("from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
         int proc = fork();
         if (proc == 0) {//in child
-            printf("name = %s\n", fileName);
-            int fileFd = open(fileName);
-            perror("error");
-            printf("file fd = %d\n", fileFd);
-            char b[1000];
+            int fileFd = open(fileName, O_RDONLY);
             while (1) {
-                ssize_t bytes = read(fileFd, b, 1000);
-                printf("bytes = %d\n", bytes);
-                perror("nooooooo");
-                if (bytes <= 0) break;
-                ssize_t wr = write(fd, b, 1000);
-                printf("wr = %d\n", wr);
-
-/*                ssize_t rb = buf_fill(fileFd, buf, MAX_SIZE_BUFFER);
-                printf("rb = %d\n", rb);
+                ssize_t rb = buf_fill(fileFd, buf, MAX_SIZE_BUFFER);
                 if (rb <= 0) break;
-                buf_flush(fd, buf, MAX_SIZE_BUFFER);*/
+                buf_flush(fd, buf, MAX_SIZE_BUFFER);
             }
             close(fileFd);
             close(fd);
